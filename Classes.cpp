31e8395139482071e7ae3f63130eab84ef8c6b37
Hritik <hritikkumar2011@gmail.com>
2022-08-27 19:21:05 +0530

    School

---
+++ Classes.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+class school{
+    private:
+    int pin;
+    int mobile_no;
+    public:
+    int ID;
+    int classNo;
+    int setdata(int pin1,int mobile_no1)
+    {
+        pin = pin1;
+        mobile_no = mobile_no1; 
+    }
+    int getdata(){
+    cout<<"ID = "<<ID<<endl;
+    cout<<"ClassNo = "<<classNo<<endl;
+    cout<<"PIN = "<<pin<<endl;
+    cout<<"Mobile number  = "<<mobile_no<<endl;
+}
+};
+int main() {
+    school Hritik;
+    Hritik.ID=2002;
+    Hritik.classNo=12;
+    Hritik.setdata(123,780777);
+    Hritik.getdata();
+return 0;
+}
